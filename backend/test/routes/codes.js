dc3fd852d3f74e3eb7b2f2f03e9caf7ab6d3dab7
p0ntan <pontus.akerberg@hotmail.com>
2023-09-15 16:44:16 +0200

    Add test for routes /delayed and /codes

---
+++ backend/test/routes/codes.js
@@ -0,0 +1,35 @@
+/**
+ * Test the route /tickets
+ */
+
+/*global it describe */
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const httpServer = require('../../app.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('route', () => {
+    describe('GET /codes', () => {
+        it('should get status 200 with a non empty array', (done) => {
+            chai.request(httpServer)
+                .get('/codes')
+                .end((err, res) => {
+                    const firstElement = res.body.data[0];
+
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('array');
+                    res.body.data.length.should.be.above(0);
+                    firstElement.should.have.property("Code");
+                    firstElement.should.have.property("Level3Description");
+
+                    done();
+                });
+        });
+    });
+});
