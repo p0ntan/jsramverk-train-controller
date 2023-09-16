5022ec6e283aeadf445c050c3b9363af912a6215
elemoser <elemoser@gmail.com>
2023-09-16 20:28:46 +0200

    Set up basic Vue app

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Example from './components/Example.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Hello world</h1>
+  </header>
+  <main>
+    <Example />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
