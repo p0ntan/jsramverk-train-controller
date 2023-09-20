cb7801397f39067843f67cd8c7f1698ad9ffe5a2
elemoser <elemoser@gmail.com>
2023-09-20 10:22:17 +0200

    Add code for tickter view

---
+++ frontend-vue/src/components/TicketView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ticket-container">
+        <div class="ticket">
+            <a href="" id="back">&#8592; Tillbaka</a>
+            <h1>Nytt ärende #<span id="new-ticket-id"></span></h1>
+            <p><strong>Försenad:</strong></p>
+            <form id="new-ticket-form">
+                <label>Orsakskod</label><br>
+                <select id="reason-code"></select><br><br>
+                <input type="submit" value="Skapa nytt ärende" />
+            </form>
+        </div>
+        <br>
+        <div class="old-tickets" id="old-tickets">
+            <h2>Befintliga ärenden</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
