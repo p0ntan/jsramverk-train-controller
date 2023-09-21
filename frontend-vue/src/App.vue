ec5403af9bceaf3bba1e1cedb4cd3d69f0593cbd
elemoser <elemoser@gmail.com>
2023-09-21 14:04:11 +0200

    Address warning in console

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -7,20 +7,14 @@ import TrainsView from './components/TrainsView.vue';
 import TicketView from './components/TicketView.vue';
 
 export default{
-  data() {
-    return {
-      currentComponent: TrainsView
-    };
-  },
   components: {
     TrainsView,
     TicketView
   },
-  created() {
-    const trainInStorage = sessionStorage.getItem("train");
-
-    if (trainInStorage) {
-      this.currentComponent = TicketView;
+  computed: {
+    currentComponent() {
+      const trainInStorage = sessionStorage.getItem("train");
+      return trainInStorage ? TicketView : TrainsView;
     }
   }
 }
