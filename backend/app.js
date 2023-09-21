2f00b17e144cea3b046ca5910b52c37a73a493cc
p0ntan <pontus.akerberg@hotmail.com>
2023-09-21 09:31:24 +0200

    Change cors for io to '*', will change this later to correct url

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 const io = require("socket.io")(httpServer, {
     cors: {
-        origin: "http://localhost:9000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
