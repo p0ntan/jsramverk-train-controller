c49e8aba404723c69969f35f546d4f219f3ca55f
elemoser <elemoser@gmail.com>
2023-09-22 15:32:16 +0200

    Update baseUrl for e2e tests

--- frontend-vue/cypress.config.js
+++ frontend-vue/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:5173',
+        baseUrl: 'https://www.student.bth.se/~elmo22/editor',
     }
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
