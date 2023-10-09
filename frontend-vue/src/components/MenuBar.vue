cee4d097a3a25f3f0683ab37999436e2ff9a14af
p0ntan <pontus.akerberg@hotmail.com>
2023-10-09 16:02:00 +0200

    Add login/register form and logic

---
+++ frontend-vue/src/components/MenuBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="this.$store.jwt">
+    Här är den satt
+  </div>
+  <div v-else>
+    <button @click="logIn">Logga in</button>
+    <button @click="register">Registera</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: [
+    'action'
+  ],
+  methods: {
+    logIn() {
+      this.$emit('action', 'login')
+    },
+    register() {
+      this.$emit('action','register')
+    }
+  }
+}
+</script>
--- frontend-vue/src/components/TrainsView.vue
+++ frontend-vue/src/components/TrainsView.vue
@@ -1,11 +1,50 @@
-<script setup>
-import DelayedTable from './DelayedTable.vue'
-import DelayedMap from './DelayedMap.vue'
-</script>
-
 <template>
   <div class="container">
-    <DelayedTable />
+    <div>
+      <MenuBar @action="renderForm"/>
+      <DelayedTable />
+    </div>
     <DelayedMap />
+    <UserForm v-if="isVisible" :formType="currentForm" v-model="isVisible"/>
   </div>
 </template>
+
+<script>
+import DelayedTable from './DelayedTable.vue'
+import DelayedMap from './DelayedMap.vue'
+import MenuBar from './MenuBar.vue'
+import UserForm from './UserForm.vue'
+
+export default {
+  data() {
+    return {
+      isVisible: false,
+      formToUse: null
+    };
+  },
+  components: {
+    DelayedTable,
+    DelayedMap,
+    MenuBar,
+    UserForm
+  },
+  methods: {
+    renderForm(form) {
+      // form is either 'login' or 'register'
+      switch (form) {
+        case "login":
+          this.isVisible = true;
+          this.currentForm = "login";
+          break;
+        case "register":
+          this.isVisible = true;
+          this.currentForm = "register";
+          break;
+        default:
+          this.isVisible = false;
+          this.currentForm = null;
+      }
+    }
+  }
+}
+</script>
