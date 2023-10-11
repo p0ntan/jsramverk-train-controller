cd1b6c53de80b397d592c2ed0aea9032b73ae7de
elemoser <elemoser@gmail.com>
2023-10-11 14:15:16 +0200

    Split up OldTicket component

--- frontend-vue/src/components/OldTickets.vue
+++ frontend-vue/src/components/OldTickets.vue
@@ -3,30 +3,14 @@
     <h2>Befintliga ärenden</h2>
     <div v-if="this.oldTickets">
       <div v-for="ticket in this.oldTickets" :key="ticket">
-        <div v-if="this.editItem == ticket._id">
-          <form @submit.prevent="saveEdit" v-if="codes">
-            <span>{{ ticket._id }} - </span>
-            <select v-model="newCode">
-              <option v-for="code in codes" :key="code" :value="code.Code">
-                {{ code.Code }} - {{ code.Level3Description }}
-              </option>
-            </select>
-            <span> - {{ ticket.trainnumber }} - {{ ticket.traindate }}</span>
-            <button type="submit">save</button>
-            <button @click="editItem = null">cancel</button>
-          </form>
-        </div>
-        <div v-else>
-          <span>{{ ticket._id }} - {{ ticket.code }} - {{ ticket.trainnumber }} - {{ ticket.traindate }}</span>
-          <button @click="editTicket(ticket._id, ticket.code)">edit</button>
-        </div>
+        <SingleTicket :ticket="ticket" :fetchTickets="fetchTickets"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { nextTick } from 'process'
+import SingleTicket from './SingleTicket.vue'
 
 const graphqlURL = import.meta.env.VITE_GRAPHQL_URL
 // Define data needed from backend
@@ -40,62 +24,18 @@ const queryTickets = `{
 }`
 
 export default {
+  components: {
+    SingleTicket
+  },
   data() {
     return {
-      oldTickets: [],
-      editItem: null,
-      currentCode: '',
-      newCode: '',
-      codes: null
+      oldTickets: []
     }
   },
   created() {
     this.fetchTickets()
   },
   methods: {
-    editTicket(ticketId, ticketCode) {
-      // Function to edit the value for Code in the existing tickets
-      // Takes the ticket id and the current ticket code as argument
-      this.codes = this.$store.codes
-      this.editItem = ticketId
-      this.currentCode = ticketCode
-      this.newCode = ticketCode
-    },
-    // TODO Add functionality to delete a ticket
-    saveEdit() {
-      // Function to save the edit made to a ticket
-      if (this.newCode != this.currentCode) {
-        const mutationUpdateTicket = `mutation {
-        updateTicket (
-          _id: "${this.editItem}",
-          code: "${this.newCode}"
-          ){_id}
-        }`
-
-        try {
-          // Fetch data via graphql
-          fetch(`${graphqlURL}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'x-access-token': this.$store.jwt
-          },
-          body: JSON.stringify({ query: mutationUpdateTicket })
-          })
-          .then(response => response.json())
-          .then(result => {
-            console.log(result)
-            this.fetchTickets()
-          })
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-
-      // reset variable
-        this.editItem = null
-    },
     fetchTickets() {
       try {
         // Fetch data via graphql
@@ -109,7 +49,6 @@ export default {
         })
         .then(response => response.json())
         .then(data => {
-          // Store received data in component variable
           this.oldTickets = data.data.tickets
         })
       } catch (error) {
---
+++ frontend-vue/src/components/SingleTicket.vue
@@ -0,0 +1,92 @@
+<template>
+    <div v-if="this.editItem == ticket._id">
+    <form @submit.prevent="saveEdit" v-if="codes">
+        <span>{{ ticket._id }} - </span>
+        <select v-model="newCode">
+        <option v-for="code in codes" :key="code" :value="code.Code">
+            {{ code.Code }} - {{ code.Level3Description }}
+        </option>
+        </select>
+        <span> - {{ ticket.trainnumber }} - {{ ticket.traindate }}</span>
+        <button type="submit">save</button>
+        <button @click="editItem = null">cancel</button>
+    </form>
+    </div>
+    <div v-else>
+    <span>{{ ticket._id }} - {{ ticket.code }} - {{ ticket.trainnumber }} - {{ ticket.traindate }}</span>
+    <button @click="editTicket(ticket._id, ticket.code)">edit</button>
+    </div>
+</template>
+
+<script>
+// import { io } from "socket.io-client"
+// const baseURL = import.meta.env.VITE_BASE_URL
+const graphqlURL = import.meta.env.VITE_GRAPHQL_URL
+
+export default {
+    props: [
+      'ticket',
+      'fetchTickets'
+    ],
+    data() {
+      return {
+        editItem: null,
+        currentCode: '',
+        newCode: '',
+        codes: null
+      }
+    },
+    methods: {
+      editTicket(ticketId, ticketCode) {
+        // Function to edit the value for Code in the existing tickets
+        // Takes the ticket id and the current ticket code as argument
+        this.codes = this.$store.codes
+        this.editItem = ticketId
+        this.currentCode = ticketCode
+        this.newCode = ticketCode
+
+        // const socket = io(`${baseURL}/`)
+        // socket.emit("edit", ticketId)
+        // // create unique room
+        // socket.emit("create", data[_id])
+      },
+      // TODO Add functionality to delete a ticket
+      saveEdit() {
+        // Function to save the edit made to a ticket
+        if (this.newCode != this.currentCode) {
+          const mutationUpdateTicket = `mutation {
+          updateTicket (
+            _id: "${this.editItem}",
+            code: "${this.newCode}"
+            ){_id}
+          }`
+
+          try {
+            // Fetch data via graphql
+            fetch(`${graphqlURL}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'x-access-token': this.$store.jwt
+            },
+            body: JSON.stringify({ query: mutationUpdateTicket })
+            })
+            .then(response => response.json())
+            .then(result => {
+              if (result.errors) {
+                window.alert(result.errors[0].message)
+              }
+              this.fetchTickets()
+            })
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        }
+
+        // reset variable
+          this.editItem = null
+      }
+    }
+}
+</script>
