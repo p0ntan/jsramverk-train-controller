5ec187d21467853913557ab7d92c5f43091b2948
elemoser <elemoser@gmail.com>
2023-10-13 14:11:33 +0200

    Fix test check navigation

--- frontend-vue/cypress/e2e/check_navigation.cy.js
+++ frontend-vue/cypress/e2e/check_navigation.cy.js
@@ -9,7 +9,7 @@ it('works successfully', () => {
     cy.get('.delayed-trains')
     .find('div').first().click()
 
-    cy.contains('h1', 'Nytt ärende').should('exist');
+    cy.contains('h2', 'Befintliga ärenden').should('exist');
 
     cy.get('a').click()
 })
