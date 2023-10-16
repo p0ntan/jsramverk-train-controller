d5550a5c6f9fba7f17fbd3fe26fbce31e013086f
elemoser <elemoser@gmail.com>
2023-10-16 16:35:43 +0200

    Replace navigation test

---
+++ frontend-vue/cypress/e2e/check_tickets_login.cy.js
@@ -0,0 +1,25 @@
+describe('Visit tickets while logged in', () => {
+it('works successfully', () => {
+    const user = {
+        username: Cypress.env('user1'),
+        password: Cypress.env('pw1')
+    }
+    // Bypass the login
+    cy.bypassLogin(user)
+
+    // Check we are on train view
+    cy.contains('h1', 'Försenade Tåg').should('exist')
+
+    // Check ticket button exists
+    cy.get('.delayed-trains').find('.train').first().find('button').should('exist')
+
+    // Click on button and navigate to ticket view
+    cy.get('.delayed-trains').find('.train').first().find('button').click()
+    cy.url().should('contain', '/tickets')
+    cy.contains('h2', 'Befintliga ärenden').should('exist');
+
+    // Navigate back to train view
+    cy.get('a').click()
+    cy.contains('h1', 'Försenade Tåg').should('exist')
+})
+})
