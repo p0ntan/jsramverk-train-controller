315d48c71db123704be69ccf6b97e93e6deee1c6
elemoser <elemoser@gmail.com>
2023-10-17 13:33:40 +0200

    Run all edit tests

--- frontend-vue/cypress/e2e/test_edit.cy.js
+++ frontend-vue/cypress/e2e/test_edit.cy.js
@@ -11,66 +11,65 @@ describe('Test editing tickets', () => {
         cy.get('.delayed-trains').find('.train').first().find('button').click()
         cy.url().should('contain', '/tickets')
     });
-    // it('edit button exists', () => {
-    //     // Check that edit button is visible
-    //     cy.get('button').contains('edit').should('exist')
+    it('edit button exists', () => {
+        // Check that edit button is visible
+        cy.get('button').contains('edit').should('exist')
 
-    //     // Check that the options to edit the code appear
-    //     cy.get('#old-tickets').find('button').first().click()
-    //     cy.get('#old-tickets').find('form').should('exist')
-    //     cy.get('#old-tickets').find('option').its('length').should('be.gt', 1)
-    //     cy.get('#old-tickets').find('button').contains('save').should('exist')
-    //     cy.get('#old-tickets').find('button').contains('cancel').should('exist')
+        // Check that the options to edit the code appear
+        cy.get('#old-tickets').find('button').first().click()
+        cy.get('#old-tickets').find('form').should('exist')
+        cy.get('#old-tickets').find('option').its('length').should('be.gt', 1)
+        cy.get('#old-tickets').find('button').contains('save').should('exist')
+        cy.get('#old-tickets').find('button').contains('cancel').should('exist')
 
-    //     // Check that cancel button works
-    //     cy.get('#old-tickets').find('button').contains('cancel').click()
-    //     cy.get('#old-tickets').find('button').contains('save').should('not.exist')
-    //     cy.get('#old-tickets').find('button').contains('cancel').should('not.exist')
-    // });
-    // it('edit one at a time', () => {
-    //     // Check that only one ticket at a time is edited
-    //     cy.get('#old-tickets').find('button').last().click()
-    //     cy.get('#old-tickets').find('button').first().click()
-    //     cy.get('#old-tickets').find('button').contains('save').its('length').should('be.lte', 1)
-    //     cy.get('#old-tickets').find('button').contains('cancel').its('length').should('be.lte', 1)
+        // Check that cancel button works
+        cy.get('#old-tickets').find('button').contains('cancel').click()
+        cy.get('#old-tickets').find('button').contains('save').should('not.exist')
+        cy.get('#old-tickets').find('button').contains('cancel').should('not.exist')
+    });
+    it('edit one at a time', () => {
+        // Check that only one ticket at a time is edited
+        cy.get('#old-tickets').find('button').last().click()
+        cy.get('#old-tickets').find('button').first().click()
+        cy.get('#old-tickets').find('button').contains('save').its('length').should('be.lte', 1)
+        cy.get('#old-tickets').find('button').contains('cancel').its('length').should('be.lte', 1)
 
-    //     // Close ticket
-    //     cy.get('#old-tickets').find('button').contains('cancel').click()
-    //     cy.get('#old-tickets').find('button').contains('save').should('not.exist')
-    //     cy.get('#old-tickets').find('button').contains('cancel').should('not.exist')
-    // });
-    // it('save edit button works', () => {
-    //     // Check that the save button works
-    //     cy.get('#old-tickets').find('button').last().click()
+        // Close ticket
+        cy.get('#old-tickets').find('button').contains('cancel').click()
+        cy.get('#old-tickets').find('button').contains('save').should('not.exist')
+        cy.get('#old-tickets').find('button').contains('cancel').should('not.exist')
+    });
+    it('save edit button works', () => {
+        // Check that the save button works
+        cy.get('#old-tickets').find('button').last().click()
 
-    //     cy.get('#old-tickets').get('select').find('option:selected').invoke('val').then(val => {
-    //         cy.log(val)
+        cy.get('#old-tickets').get('select').find('option:selected').invoke('val').then(val => {
+            cy.log(val)
 
-    //         // Select another option (ANA002)
-    //         const edit = 'ANA002'
+            // Select another option (ANA002)
+            const edit = 'ANA002'
 
-    //         cy.get('#old-tickets').find('select').select(edit)
-    //         cy.get('#old-tickets').find('button[type=submit]').click()
+            cy.get('#old-tickets').find('select').select(edit)
+            cy.get('#old-tickets').find('button[type=submit]').click()
 
-    //         // Check change occurred
-    //         cy.get('#old-tickets').last().contains(edit)
+            // Check change occurred
+            cy.get('#old-tickets').last().contains(edit)
 
-    //         // Change back
-    //         cy.get('#old-tickets').find('select').select(val)
-    //         cy.get('#old-tickets').find('button[type=submit]').click()
-    //         cy.get('#old-tickets').last().contains(val)
-    //     });
-    // });
-    // it('userA enters edit mode and leave page via navigation', () => {
-    //     // Click on edit button and leave
-    //     cy.get('#old-tickets').find('button').contains('edit').first().click()
-    // });
-    // it('userB can edit previously blocked ticket', () => {
-    //     // Edit button is not blocked ticket
-    //     cy.get('#old-tickets').find('div').find('div').first()
-    //     .find('button').contains('edit').should('exist')
-    // });
-    // TODO check this part
+            // Change back
+            cy.get('#old-tickets').find('select').select(val)
+            cy.get('#old-tickets').find('button[type=submit]').click()
+            cy.get('#old-tickets').last().contains(val)
+        });
+    });
+    it('userA enters edit mode and leave page via navigation', () => {
+        // Click on edit button and leave
+        cy.get('#old-tickets').find('button').contains('edit').first().click()
+    });
+    it('userB can edit previously blocked ticket', () => {
+        // Edit button is not blocked ticket
+        cy.get('#old-tickets').find('div').find('div').first()
+        .find('button').contains('edit').should('exist')
+    });
     it('userA enters edit mode and refreshes the page', () => {
         // Click on edit button and leave
         cy.get('#old-tickets').find('button').contains('edit').first().click()
