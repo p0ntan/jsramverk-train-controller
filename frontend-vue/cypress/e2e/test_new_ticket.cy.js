745f4d5b00bfacc536a5753449d819f5ec0c8893
elemoser <elemoser@gmail.com>
2023-10-17 09:15:01 +0200

    Update new ticket test

--- frontend-vue/cypress/e2e/test_new_ticket.cy.js
+++ frontend-vue/cypress/e2e/test_new_ticket.cy.js
@@ -8,36 +8,38 @@ describe('New ticket', () => {
         cy.bypassLogin(user);
     });
     it('created successfully', () => {
-        // The uncommented code modifies the deployed db
-        // TODO look up how to use a test database
-
         // Save train number in the first element
         let trainNumber
 
-        cy.get('.train-number').first().then(($element) => {
+        cy.get('.delayed-trains').find('.train-number').last().then(($element) => {
             trainNumber = $element.text()
             expect(trainNumber).to.exist
 
             // Navigate to the TicketView for that train
-            cy.get('.delayed-trains').find('.train').first().find('button').click()
+            cy.get('.delayed-trains').find('.train').last().find('button').click()
             cy.url().should('contain', '/tickets')
+            cy.contains(trainNumber)
 
             // Form to create new Ticket should exist
             cy.get('form').should('exist')
             cy.get('select').should('exist')
             cy.get('select').find('option').its('length').should('be.gt', 1)
 
+            // The code below modifies the db
+            // Make sure to use the test database // TODO look up alternative
+
             // Select option ANA031
-            // cy.get('select').select('ANA031 - Kort tåg')
+            cy.get('select').contains('ANA031').should('exist')
+            cy.get('select').select('ANA031')
 
             // Submit choice
             cy.get('input[type="submit"]').should('exist')
-            // cy.get('input[type="submit"]').click()
+            cy.get('input[type="submit"]').click()
 
             // Check ticket added to ticket list
-            // cy.get('.old-tickets').find('div').last().should('exist')
-            // cy.get('.old-tickets').find('div').last().invoke('text').should('include', 'ANA031')
-            // cy.get('.old-tickets').find('div').last().should('contain', trainNumber)
+            cy.get('.old-tickets').find('div').last().should('exist')
+            cy.get('.old-tickets').find('div').last().invoke('text').should('include', 'ANA031')
+            cy.get('.old-tickets').find('div').last().should('contain', trainNumber)
         })
     });
     after(() => {
